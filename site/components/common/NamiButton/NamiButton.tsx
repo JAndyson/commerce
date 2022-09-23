bc46c8b5248099b03c7a86750974ded83f0be039
Jacob Anderson <63214293+JAndyson@users.noreply.github.com>
2022-09-22 20:09:28 -0500

    Nami button implementation

---
+++ site/components/common/NamiButton/NamiButton.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { Wallet } from '@harmonicpool/cardano-wallet-interface'
+
+export default class Home extends React.Component<
+  {},
+  { currentDelegation: {}; NamiHasBeenInitialized: boolean }
+> {
+  constructor(props) {
+    super(props)
+
+    // set your key once then you are free to go
+    Wallet.setBlockfrost('testnetWS8ic2FQlQxLBK0m58WVhe5e4dKgaQBp')
+
+    this.state = {
+      currentDelegation: {},
+      NamiHasBeenInitialized: false,
+    }
+  }
+
+  async componentDidMount() {
+    await Wallet.enable(Wallet.Names.Nami)
+    // now Wallet.Nami is accessible
+
+    this.setState({
+      currentDelegation: await Wallet.Nami.getCurrentUserDelegation(),
+      NamiHasBeenInitialized: true,
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {!this.state.NamiHasBeenInitialized ? (
+          <p style={txtSty}>looking for Nami... 👀</p>
+        ) : this.state.currentDelegation.pool_id === '<your pool id>' ? (
+          <p style={txtSty}>Thank you for your support &#9829;</p>
+        ) : (
+          <button onClick={() => Wallet.enable(Wallet.Names.Nami)}>
+            Connect
+          </button>
+        )}
+      </div>
+    )
+  }
+}
+
+const containerSty = {
+  position: 'absolute',
+  height: '100vh',
+  width: '100vw',
+  top: 0,
+  left: 0,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const txtSty = {
+  fontFamily: 'Arial, sans-serif',
+  fontSize: '5vh',
+}
